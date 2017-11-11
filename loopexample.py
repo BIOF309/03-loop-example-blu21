7fc6817cb5c421c4677e62e3d81fc72caedd6819
blu21 <gobillylu@gmail.com>
2017-11-11 00:37:53 -0500

    updated loop example homework

--- loopexample.py
+++ loopexample.py
@@ -3,6 +3,7 @@
 #A for loop can used in a fluorescence assay for example to find the sample with the highest and lowest fluorescence signal. The loop can take in a list of all the fluorescence values and determine the largest and smallest values from the list.
 
 print ("This program uses a for loop to find the highest and lowest fluorescence values from a list of fluorescence values obtained from some sort of fluorescence assay.")
+
 #Allows users to input numbers as string types, which will then be split into a list of strings by the split method. The float function will convert the numbers to the float type.
 Fval = [float (x) for x in input ('Enter your fluorescence values (separate each value by a space):' ).split()]
 
@@ -16,6 +17,6 @@ for i in Fval:
     if i < minF:
         minF = i
 
-print ("Your fluorescence values are ", Fval)
+print ("Your fluorescence values are ", Fval) #prints out the list of fluorescence values
 print ("The highest fluorence value is " + str(maxF)) #prints out highest value
 print ("The lowest fluorescence value is " + str (minF)) #prints out lowest value
